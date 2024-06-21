188a56abc11ada58f9fc353dbf56dd3bd6aab3d2
Mohamadrzaa2 <mohamadrzaa2@gmail.com>
2024-06-21 19:29:05 +0330

    lklk

--- models/logistic_model.py
+++ models/logistic_model.py
@@ -24,15 +24,15 @@ def logistic_tester(model, x_test, y_test):
     return classification_report(y_test, predict)
 
 
-data = load_iris()
-X = data.data
-y = data.target
-
-X_train, X_test, y_train, y_test = data_splitter(X, y, test_size=3)
-
-model = logistic_model_maker()
-
-model = logistic_trainer(model, X_train, y_train)
-
-report = logistic_tester(model, X_test, y_test)
-print(report)
+# data = load_iris()
+# X = data.data
+# y = data.target
+#
+# X_train, X_test, y_train, y_test = data_splitter(X, y, test_size=3)
+#
+# model = logistic_model_maker()
+#
+# model = logistic_trainer(model, X_train, y_train)
+#
+# report = logistic_tester(model, X_test, y_test)
+# print(report)
