9597c38af6c08beff2d6e6ea1c047a46d48a0731
Student <roozbeh.mo670@gmail.com>
2024-06-28 18:41:33 +0330

    Agent Mokhtarzadeh

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,6 @@ from models.rf_model import *
 
 st.title("Machine Learning Dashboard")
 
-
 style = "<style> .appview-container .main .block-container{ max-width: 100%; padding: 50px;}</style>"""
 st.markdown(body=style, unsafe_allow_html=True)
 
@@ -28,7 +27,7 @@ if dataset_file:
     X = df.drop(columns=y)
     y = df[y]
 
-    col1, col2 = st.columns([0.8,0.5])
+    col1, col2 = st.columns([0.8, 0.5])
 
     with col1:
         st.write(X)
@@ -36,12 +35,12 @@ if dataset_file:
         st.write(y)
 
 if st.checkbox("Train Test Split"):
-    test_size = int(st.slider("Test Size %",5,50,20))
+    test_size = int(st.slider("Test Size %", 5, 50, 20))
     x_train, x_test, y_train, y_test = data_splitter(X, y, test_size=test_size)
 
 model_name = st.selectbox("Select Model",
                           ["LogisticRegression", "KNeighborsClassifier", "SVC", "DecisionTreeClassifier",
-                           "MLPClassifier","RandomForestClassifier"])
+                           "MLPClassifier", "RandomForestClassifier"])
 
 match model_name:
     case "LogisticRegression":
@@ -67,24 +66,22 @@ match model_name:
         mlp_hidden_layers = eval(st.text_input("hidden_layer_sizes", (100,)))
         activation = st.selectbox("activation", ["identity", "logistic", "relu", "tanh"], 2)
         solver = st.selectbox("solver", ["lbfgs", "sgd", "adam"], 2)
-        verbose=st.selectbox("verbose", [False, True],0)
+        verbose = st.selectbox("verbose", [False, True], 0)
         print(verbose)
         max_iter = int(st.text_input("max_iter", 200))
         print(max_iter)
-        learning_rate_init=float(st.text_input("learning_rate_init", 0.001))
+        learning_rate_init = float(st.text_input("learning_rate_init", 0.001))
         print(learning_rate_init)
         print(type(learning_rate_init))
-        model = mlp_model_maker(mlp_hidden_layers, activation, solver,learning_rate_init, max_iter, verbose)
-
+        model = mlp_model_maker(mlp_hidden_layers, activation, solver, learning_rate_init, max_iter, verbose)
 
     case "RandomForestClassifier":
-        rf_estimator = st.text_input("n_estimators" , 100)
-        max_depth = st.text_input("Max Depth", "None")
-        min_samples_split =int(st.slider("Min Sample Split",2,20,2))
-        min_samples_leaf = int(st.slider("Min Sample Leaf",1,20,2))
-        criterion = st.selectbox("criterion", ["gini", "entropy", "log_loss"] , 0)
-        model = rf_model_maker(rf_estimator, criterion, max_depth, min_samples_split, min_samples_leaf)
-
+        rf_estimator = eval(st.text_input("n_estimators", (100)))
+        max_depth = st.text_input("Max Depth", None)
+        min_samples_split = int(st.slider("Min Sample Split", 2, 20, 2))
+        min_samples_leaf = int(st.slider("Min Sample Leaf", 1, 20, 2))
+        criterion = st.selectbox("criterion", ["gini", "entropy", "log_loss"], 0)
+        model = rf_model_maker(rf_estimator, max_depth, min_samples_split, min_samples_leaf, criterion)
 
 if st.button("Train"):
     st.toast("Wait for training ...")
@@ -98,10 +95,8 @@ if st.button("Save Model"):
         pickle.dump(model, file)
 
 if st.button("Test"):
-        st.toast("Wait for testing ...")
+    st.toast("Wait for testing ...")
 
-        mlp_trainer(model, x_train, y_train)
-        st.write(mlp_tester(model, x_test, y_test))
-        st.toast("Done")
-
-
+    mlp_trainer(model, x_train, y_train)
+    st.write(mlp_tester(model, x_test, y_test))
+    st.toast("Done")
