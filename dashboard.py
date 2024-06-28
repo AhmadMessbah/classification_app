26f2de8014afac09c96ac761b3bbdfac201c97c7
LOTFI <mohammadali80.ml@gmail.com>
2024-06-28 17:35:49 +0330

    EDITED BY LOTFI

--- dashboard.py
+++ dashboard.py
@@ -102,9 +102,15 @@ if st.button("Save Model"):
 
 if st.button("Test"):
         st.toast("Wait for testing ...")
-
+    if model_name== 'MLPClassifier':
+        mlp_trainer(model, x_train, y_train)
+        st.write(mlp_tester(model, x_test, y_test))
+        st.toast("Done")
+    else:
         svc_trainer(model, x_train, y_train)
         st.write(svc_tester(model, x_test, y_test))
         st.toast("Done")
+
+
 
 
